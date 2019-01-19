4dce846d8d8fb06523ef3392505b87c47218144f
silky <noonsilk+-noonsilk@gmail.com>
2019-01-19 20:12:12 +1100

    new logo; favicon

--- src/site.hs
+++ src/site.hs
@@ -27,6 +27,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "favicon.ico" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
