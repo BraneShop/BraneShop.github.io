3d4883703052300c614f7fd072980c136434b375
silky <noonsilk+-noonsilk@gmail.com>
2019-07-03 22:55:31 +1000

    these testimonials dont exist

--- src/site.hs
+++ src/site.hs
@@ -97,6 +97,7 @@ main = hakyll $ do
     match (fromList [ "index.html"
                     , "team.html"
                     , "contact.html"
+                    , "thesetestimonialsdontexist.html"
                     -- , "splash.html"
                     , "privacy.html"
                     , "quickstart.html"
